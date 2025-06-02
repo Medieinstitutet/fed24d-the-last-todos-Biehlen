9b93d8b3a22480bef0126a900cdee8f143d37bf3
Anna Biehl <annabiehl@hotmail.com>
2025-06-02 18:46:36 +0200

    :sparkles: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+export const Header = () => {
+    return (
+        <>
+            <header>
+                <img src="src/assets/Logo.png" alt="A white logo of a clipboard with the text To-Do List on it." />
+                <h1>Get shit done</h1>
+            </header>
+        </>
+    );
+};
