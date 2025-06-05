951e8ee49d49ed063c0f97cdc38f08b29eb61195
Anna Biehl <annabiehl@hotmail.com>
2025-06-05 21:06:05 +0200

    fix: add more todos

--- src/Data/initialTodos.ts
+++ src/Data/initialTodos.ts
@@ -5,4 +5,9 @@ export const initialTodos: Todo[] = [
     {id: Date.now() +1, title: "Study", isCompleted: false},
     {id: Date.now() +2, title: "Go for a walk", isCompleted: false},
     {id: Date.now() +3, title: "Buy a new carpet", isCompleted: false},
+    {id: Date.now() +4, title: "Buy flowers", isCompleted: false},
+    {id: Date.now() +5, title: "Do the dishes", isCompleted: false},
+    {id: Date.now() +6, title: "Book a trip", isCompleted: false},
+    {id: Date.now() +7, title: "Go swimming", isCompleted: false},
+    {id: Date.now() +8, title: "Go for a run", isCompleted: false},
 ];
